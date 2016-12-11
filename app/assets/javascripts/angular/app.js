2fc38d3b4005a6c794c880e38b4bee6dce2d26eb
Mark <mystycs@gmail.com>
2016-12-11 15:27:38 -0500

    added home and list page

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,15 +1,16 @@
 angular
   .module('tacoApp', ['ui.router', 'ngResource', 'templates'])
   .config(function($stateProvider, $urlRouterProvider) {
-     $stateProvider
-       .state('home', {
-         templateUrl: 'navbar/navbar.html',
-         controller: 'HomeController as ctrl'
-       })
-       .state('home.index', {
-         url: '/',
-         templateUrl: 'home/index.html',
-         controller: 'HomeController as ctrl'
-       })
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/index.html',
+        controller: 'HomeController as ctrl'
+      })
+      .state('list', {
+        url: '/list',
+        templateUrl: 'home/list.html',
+        controller: 'HomeController as ctrl'
+      })
     $urlRouterProvider.otherwise('/');
   });
